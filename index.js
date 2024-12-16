29803828fbfc92b7350cf4aa25f2d0fba21357f2
DAVID FROHLICH <31089662+DigitalEnforcer@users.noreply.github.com>
2024-12-16 11:54:33 -0800

    finished chromeExtention
    finished up the code and design

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-// chrome://extensions/
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads")|| "[]" )
 const tabBtn = document.getElementById("tab-btn")
 
-if (leadsFromLocalStorage) {
+if (leadsFromLocalStorage.length > 0) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
-tabBtn.addEventListener("click", function(){
+tabBtn.addEventListener("click", function(){    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
     })
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
 })
 
 function render(leads) {
@@ -36,20 +30,19 @@ function render(leads) {
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function(){
+deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
-    myLeads=[]
+    myLeads = []
     render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
 })
-
 
